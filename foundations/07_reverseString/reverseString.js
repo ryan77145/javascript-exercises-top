e2a6880bdae94616e159a3da74e5a086af9eb032
ry77145 <ryan@ryanshill.com>
2026-08-13 10:29:35 -0500

    Completed 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversedStr = str.split('').reverse().join('');
+    return reversedStr;
 };
 
 // Do not edit below this line
