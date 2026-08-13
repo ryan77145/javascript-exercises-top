4f408d7b69a551bb8f1334a69e3968bf2df81353
ry77145 <ryan@ryanshill.com>
2026-08-13 10:40:40 -0500

    Completed 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...index) {
+    let newArray = [];
+    arr.forEach((item) => {
+        if (!index.includes(item)) {
+            newArray.push(item);
+        }
+}); 
+    return newArray;
 };
 
 // Do not edit below this line
