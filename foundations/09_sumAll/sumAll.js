d57e2486e264e68333b42e8d237b775c7c0701c0
Ryan Shill <ryshill1996@gmail.com>
2026-08-13 19:17:27 -0500

    Completed 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
